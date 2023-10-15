2ef99d1a513c986bede68d5aa7f00ea09d002fcd
alexfedy <916falex@gmail.com>
2023-10-15 15:01:28 -0700

    made img absolute inteads of bg image in main of hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,10 +4,15 @@ import { motion } from "framer-motion";
 const Hero = (props) => {
   return (
     <>
-      <main
+      {/* <main
         className="text-center flex justify-center items-center mb-16 bg-[url('/home-design.jpg')] bg-fixed bg-cover bg-no-repeat w-100 h-100"
-        style={{ height: "90vh" }}>
-        <div className="flex flex-col items-center justify-center border h-full w-full p-8 bg-black bg-opacity-50 rounded-md">
+        style={{ height: "90vh" }}> */}
+      <main className="relative text-center flex justify-center items-center mb-16 w-full h-[90vh]">
+        <img
+          src="/home-design.jpg"
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+        <div className="flex flex-col items-center justify-center border h-full w-full p-8 bg-black bg-opacity-50 rounded-md z-20">
           <motion.h1
             initial={{ opacity: 0, y: 30 }}
             animate={{ opacity: 1, y: 0 }}
