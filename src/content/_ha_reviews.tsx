908fc0c12e814892ced1279e1613210016d4f295
alexfedy <916falex@gmail.com>
2025-04-11 18:15:59 -0700

    removed date from reviews, added icons

--- src/content/_ha_reviews.tsx
+++ src/content/_ha_reviews.tsx
@@ -4,6 +4,7 @@ export const ha_reviews = [
     date: "12/21/2020",
     name: "Stephanie H.",
     location: "Folsom, CA",
+    bgColor: "#BEBFC5",
     title: "Remodel or Renovate One or More Rooms",
     description:
       "CDS was wonderful to work with. Ben and Yoni are meticulous and efficient in getting the job done. They are flexible in last minute changes and was still able to complete the work in a timely fashion. I would work with them again.",
@@ -13,6 +14,7 @@ export const ha_reviews = [
     date: "6/30/2020",
     name: "Stephanie J.",
     location: "Carmichael, CA",
+    bgColor: "#B2BEB5",
     title: "Remodel a Bathroom",
     description:
       "I was impressed by the conscientiousness of the company and the workmanship was excellent. The project took a long time to complete but covid got in the way and it was worth the wait.",
@@ -22,6 +24,7 @@ export const ha_reviews = [
     date: "4/29/2020",
     name: "Terry C.",
     location: "Davis, CA",
+    bgColor: "#E5E4E2",
     title: "Remodel a Kitchen",
     description: "",
   },
@@ -29,7 +32,8 @@ export const ha_reviews = [
     stars: "5",
     date: "2/20/2019",
     name: "Vikki D.",
-    location: "",
+    location: "#C0C0C0",
+    bgColor: "#DBD7D2",
     title: "Remodel or Renovate One or More Rooms",
     description:
       "We had a very positive experience, our remodel was better than our expectation.",
@@ -39,6 +43,7 @@ export const ha_reviews = [
     date: "2/15/2019",
     name: "Michael W.",
     location: "Sacramento, CA",
+    bgColor: "#DBD7D2",
     title: "Remodel or Renovate One or More Rooms",
     description:
       "California Design Solutions has skilled craftsmen and proven expertise in kitchen/bathroom remodeling, cabinets, tile, wall/ceiling texturing, electrical, plumbing, carpentry, and interior painting. We have used their services multiple times. The owners, Ben and Yanis, are particularly good at offering suggestions - we included a couple of them (pantry expansion and wall sconces) during the major remodel of our house.",
@@ -48,6 +53,7 @@ export const ha_reviews = [
     date: "2/4/2019",
     name: "Donita S.",
     location: "Folsom, CA",
+    bgColor: "#B2BEB5",
     title: "Remodel or Renovate One or More Rooms",
     description:
       "We loved the entire crew. All of his men were highly professional and did great work. Worked long hours. It took extra time to wrap up the job with some final items but were very responsive. We would recommend them to anyone. Would hire them again.",
--- src/content/_hz_reviews.tsx
+++ src/content/_hz_reviews.tsx
@@ -3,6 +3,7 @@ export const hz_reviews = [
     stars: "5",
     date: "February 13, 2023",
     name: "regarberson",
+    bgColor: "#B2BEB5",
     description:
       "Kim and Ben did a phenomenal job renovating our entire house. We didn't know where to start, and Kim provided excellent guidance to design the home of our dreams. She planned our entire kitchen layout (including all brand new cabinets) and helped us choose everything from flooring, countertops, tiling, kitchen and bath faucets, paint colors, lighting, doors, and furniture. Ben and his team do top quality work and really pay attention to every detail. They made sure the job was done right as if it were their own home. They even do all the required permitting, which is a pain in our town. Everything turned out beautifully! Highly recommend!",
   },
@@ -10,12 +11,14 @@ export const hz_reviews = [
     stars: "5",
     date: "December 11, 2022",
     name: "Carrie Fredine",
+    bgColor: "#DBD7D2",
     description:
       "I don't even know where to start with how fabulous Kimberly and the rest of the team were in helping us remodel both of our bathrooms.  As soon as I met Kimberly and Ben, I knew we had found our team.  So professional and great with communication and detail.  Always on time and plus excellent craftsmanship.  Kimberly met with us, helped us pick out all items, and was so knowledgeable.  Yanis, was our main contact for construction and such a nice man and his team can do anything! If I ever have another project in the future this will be our team.  So thankful I found them on houzz.com.",
   },
   {
     stars: "5",
     date: "September 27, 2022",
+    bgColor: "#E5E4E2",
     name: "n50gndG1",
     description:
       "It is my honor and privilege to recommend Kim West.  I began my long association with her nearly 20 years ago when I needed window treatments for my dining room. This was the start of many years and many projects of working together on my townhome and now it is complete.  The biggest compliment I can give a designer I can give to Kim: her work has withstood the test of time!  Big bonus: her staff is incredible and beyond reproach - so very important - and I was very pleased to recommend her and them to my daughter and son in law who redid their master bath and kitchen with stunning results.",
@@ -23,6 +26,7 @@ export const hz_reviews = [
   {
     stars: "5",
     date: "March 2, 2022",
+    bgColor: "#C0C0C0",
     name: "HU-502549556",
     description:
       "Kimberley, Ben, and the team all did an excellent job remodeling the one bathroom in our modest 1930s home. Kim helped us choose a fabulous Italian tile for the bathroom floor and shower wall, as well as matching tiles for the shower, baseboard, and window trim. The two glass shower walls make our tiny bathroom feel much larger than it is. And the overall design, while modern, also connects to the look and feel of the 1930s. It was helpful to have Kim and Ben come out together for the first site visit to look at the existing layout, take measurements, and talk through ideas for the remodel. In addition to the tile, Kim helped us choose tile grout, cabinets and pulls, bathroom vanity, mirror, towel bars, faucet, shower head, and paint colors. Ben’s team was fantastic, especially Serge and the meticulous tile layer, Andrew. Yanis, our primary contact during the remodel, provided excellent communication, coordination of work, and customer support. We love our new bathroom, and we highly recommend the work of West Designs and California Design Solutions.",
@@ -30,6 +34,7 @@ export const hz_reviews = [
   {
     stars: "5",
     date: "February 16, 2022",
+    bgColor: "#B2BEB5",
     name: "jeanetterainey",
     description:
       "Kimberly and her team did a fantastic job. I couldn’t be happier with the finished project. It was definitely a collaboration and with Kimberly’s advice and guidance I got a kitchen that I can’t stop raving about.",
@@ -37,6 +42,7 @@ export const hz_reviews = [
   {
     stars: "5",
     date: "January 1, 2021",
+    bgColor: "#DBD7D2",
     name: "lauraywong",
     description:
       "Kimberley and Ben came out to give me an estimate for a kitchen renovation and I was immediately struck by their attention to detail when measuring out the kitchen and interest in what my vision/style was for the project.  They installed cabinets, countertops and appliances for my home.  They also built a media niche and mantle to match the kitchen. When there was not enough flooring to cover up the pre-existing footprint of the island, their team of installers were able to do the job.  When I asked for special outlets and lighting they did their best to accommodate my design vision and style.  All the installers who came to the house were polite, careful and professional.  There were points in the process when I had lots of questions; Kimberley and Ben were patient and kind to listen and wait for me to 'figure out' what I wanted.  The guys even pushed to finish the cooktop and sink in the kitchen so I could have a mostly functional kitchen for the Thanksgiving holiday.  I was so appreciative of that consideration.  Overall, I had a great experience working with K+B Designer/Builder.",
@@ -44,6 +50,7 @@ export const hz_reviews = [
   {
     stars: "5",
     date: "July 23, 2020",
+    bgColor: "#BEBFC5",
     name: "Prosource Whoesale",
     description:
       "I have assisted Kimberly West on numerous projects. She is the ultimate professional in dealing with her clients.  Each individual receives a personalized design.  Kimberly is very thorough in including details of the customers dream for the project. Kimberly's vested interest with the clients budget and her contact with personal observation streamlines clients decisions.  Her vast experience makes each project a one of a kind. Kimberly is a skilled professional and a  pleasure to work with.",
@@ -52,6 +59,7 @@ export const hz_reviews = [
     stars: "5",
     date: "June 23, 2020",
     name: "HU-823414104",
+    bgColor: "#C0C0C0",
     description:
       "It has always been exciting to brainstorm home decorating ideas with Kimberley. Her suggestions  are creative and practical, while still eye-catching and unique. It is pleasurable to engage with her.",
   },
@@ -59,6 +67,7 @@ export const hz_reviews = [
     stars: "5",
     date: "May 22, 2020",
     name: "James Moultrie",
+    bgColor: "#E5E4E2",
     description:
       "Kimberly West, The care and professionalism she work in is impeccable. Attention to details and preciseness. I personally recommend miss West for your bath and kitchen design.",
   },
@@ -66,6 +75,7 @@ export const hz_reviews = [
     stars: "5",
     date: "April 16, 2020",
     name: "Chanie Hellwig",
+    bgColor: "#B2BEB5",
     description:
       "As a supplier of material for Kimberley's design vision, I can tell you she has a great eye for design! I highly recommend hiring her on your upcoming design project.",
   },
@@ -73,6 +83,7 @@ export const hz_reviews = [
     stars: "5",
     date: "April 16, 2020",
     name: "Scott Harris",
+    bgColor: "#DBD7D2",
     description:
       "Kimberly listens to you.  She doesn't just come in and tell you what she wants to do.  She tries to give you want with her experience and guidance.  She gives pointers and makes suggestions and in the end, we got exactly what we wanted.  We couldn't be happier with the result and are happy to recommend her.  Her builder is the best.  The quality and craftsmanship shine through!  Trust us when we say you'll be glad you called her first!",
   },
@@ -80,6 +91,7 @@ export const hz_reviews = [
     stars: "5",
     date: "April 15, 2020",
     name: "Bill and Sue Zeidenberg",
+    bgColor: "#DBD7D2",
     description:
       "We are extremely satisfied with our new kitchen, especially with Kimberley's design: the layout of the cabinets, the refrigerator, oven, and countertops which is far more versatile and now allows two people to easily work in the kitchen. In addition, we also love the cabinets, the backsplash tiles, the floor, the paint colors, the wall and ceiling texturing as well as the end bookcase. All together, the kitchen is fantastic!  It suits our house and our lifestyle. We are grateful that most of the work was finished before we returned, and almost all the remainder of the work was completed the Monday after we returned from vacation 2 weeks early. Again, we love our new kitchen, and to us it was converted from a “pig’s ear” into a “silk purse”. Great design and great workmanship. We would recommend the work done on our kitchen.",
   },
@@ -87,6 +99,7 @@ export const hz_reviews = [
     stars: "5",
     date: "April 14, 2020",
     name: "HU-226404970",
+    bgColor: "#BEBFC5",
     description:
       "Kimberley West remodeled my kitchen in late 2019 and I am very pleased with the finished kitchen! Kimberley scheduled a consultation with me and I was very pleased with her vision, cost and timeframe to complete the work. I highly recommend this service!",
   },
@@ -94,6 +107,7 @@ export const hz_reviews = [
     stars: "5",
     date: "April 14, 2020",
     name: "dcdali2011",
+    bgColor: "#E5E4E2",
     description:
       "Kimberley and BEN had done a complete remodel of my son’s home that was just magnificent. Therefore, I couldn’t wait to have them start on my project, already knowing the great work that they do. How do I even begin to describe the magnificent spa bathroom that Kimberley and Ben and all of their craftsman have created for me. It is beyond my expectations!!  The workmanship that their craftsman demonstrated is second to none. I was amazed at how Hard they worked and with such care. They were all very kind and respectful, and it was a pleasure to have them in my home. Kimberley and BEN are simply the BEST at what they do, and they have built an incredible team of craftsman to work with them. .I have at least one more project that I have in mind in my home and I will definitely contact Kimberley and Ben again.  I Give them the highest recommendation possible.",
   },
