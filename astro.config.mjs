82322e59609515f51b8c5c6f3e00403e5b33007e
alexfedy <916falex@gmail.com>
2025-12-16 17:26:48 -0800

    added gtag to index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
+import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://cdsbuilder.com",
@@ -15,5 +16,19 @@ export default defineConfig({
     }),
     mdx(),
     sitemap(),
+    partytown({
+      config: {
+        debug: false, // Set to false in production
+        forward: ["dataLayer.push", "gtag"],
+        // Add this to resolve proxy issues
+        resolveUrl: (url) => {
+          // Don't proxy googletagmanager.com
+          if (url.hostname.includes("googletagmanager.com")) {
+            return new URL(url);
+          }
+          return url;
+        },
+      },
+    }),
   ],
 });
